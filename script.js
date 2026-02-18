3dba567b7a9fd3f7408be097a404770e7dd0b3ee
Silvia Indah <silviaindahrahayu91@gmail.com>
2026-02-18 14:05:15 +0700

    upload portfolio pertama

---
+++ script.js
@@ -0,0 +1,14 @@
+// Animasi muncul saat scroll
+const cards = document.querySelectorAll(".card");
+
+window.addEventListener("scroll", () => {
+    cards.forEach(card => {
+        const position = card.getBoundingClientRect().top;
+        const screenHeight = window.innerHeight;
+
+        if (position < screenHeight - 50) {
+            card.style.opacity = "1";
+            card.style.transform = "translateY(0)";
+        }
+    });
+});
